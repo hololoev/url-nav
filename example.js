60cdc4f94e1fbba9e667261aec00a91c8d9137ef
ilya.yurchenko <ilya.yurchenko.firefly@gmail.com>
2017-03-16 10:42:19 +0700

    Close project. It was bad experiment.

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
-const urlCalc = require('./index.js');
+const urlCalc = require('./index.js')
 
-console.log(urlCalc('http://example.com/page1', 'article1/'));
+console.log(urlCalc('http://example.com/page1', 'article1/'))
 
-console.log(urlCalc('http://example.com/page1', '../page2/article2'));
+console.log(urlCalc('http://example.com/page1', '../page2/article2'))
 
-console.log(urlCalc('http://example.com/page1/article4', '/page3/article3'));
+console.log(urlCalc('http://example.com/page1/article4', '/page3/article3'))
 
-console.log(urlCalc('http://example.com/page1', 'http://example.com/page2'));
+console.log(urlCalc('http://example.com/page1', 'http://example.com/page2'))
